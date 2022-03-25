e751cf9b89371459f2ffb622c4a1b8410524c5c0
IT20024536 <88772876+IT20024536@users.noreply.github.com>
2022-03-26 01:29:13 +0530

    initial backend with database access

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express()
+
+//bodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+const uri = "mongodb+srv://admin:admin123@supermart.ayocv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("Mongo DB connected....")).catch((err) => {
+    console.log(`Could not connect to the database ${err}`);
+    process.exit();
+});
+
+app.get('/', (req, res) => {
+  res.send("Server is running")
+})
+
+//Import routes
+const ItemRoutes = require('./routes/item.router');
+const CustomerRoutes = require('./routes/customer.router');
+
+app.use('/item', ItemRoutes);
+app.use('/customer', CustomerRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
